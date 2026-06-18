93d7896e6e0ced0958a2089406670240fee27b2c
Wallace Martins da Silva <martinsswallace@hotmail.com>
2026-06-18 15:34:31 -0300

    feat: estrutura da pagina

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+
+import About from "@/sections/About";
+import Contact from "@/sections/Contact";
+import Experience from "@/sections/Experience";
+import Projects from "@/sections/Projects";
 
 export default function Home() {
-  return (
-    <>
-      
-    </>
-  )
+    return (
+        <main>
+            <Navbar />
+
+            <About />
+
+            <Experience />
+
+            <Projects />
+
+            <Contact />
+
+            <Footer />
+        </main>
+    );
 }
